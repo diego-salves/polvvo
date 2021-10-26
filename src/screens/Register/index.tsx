8e1544e43991f59746429b1fc1f2a14a984851a4
Diego Santos <diegosantos57@outlook.com.br>
2021-10-25 23:21:28 -0300

    a

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { Modal } from 'react-native';
 
 import { Input } from "../../components/Form/Input";
 import { Button } from "../../components/Form/Button";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
-import { CategorySelect } from "../../components/Form/CategorySelect";
+import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
+
+import { CategorySelect } from '../CategorySelect';
 
 import { 
     Container,
@@ -16,9 +19,23 @@ import {
 
 export function Register(){
     const [transactionType, setTransactionType] = useState(' ');
+    const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+    const [category, setCategory] = useState({
+        key: 'category',
+        name: 'Categoria',
+    });
 
     function handleTransactionTypeSelect(type: 'up' | 'down'){
         setTransactionType(type);
+    }
+
+    function handleOpenSelectCategoryModal(){
+        setCategoryModalOpen(true);
+    }
+
+    function handleCloseSelectCategoryModal(){
+        setCategoryModalOpen(false);
     }
 
     return(
@@ -54,11 +71,22 @@ export function Register(){
                         />
                     </TransactionsTypes>
 
-                    <CategorySelect title="Categoria" />
+                    <CategorySelectButton 
+                        title={category.name}
+                        onPress={handleOpenSelectCategoryModal} 
+                    />
                 </Fields>
 
                 <Button title="Adicionar meta" />
             </Form>
+            
+            <Modal visible={categoryModalOpen}>
+                <CategorySelect
+                    category={category}
+                    setCategory={setCategory}
+                    closeSelectCategory={handleCloseSelectCategoryModal}
+                />
+            </Modal>
 
         </Container>
     );
