d57716d0ef168050cb56f3bd903646074be365c2
Diego Santos <diegosantos57@outlook.com.br>
2021-10-26 00:30:32 -0300

    auu

--- App.tsx
+++ App.tsx
@@ -13,9 +13,11 @@ import {
 //  Nunito_700Bold,
 //} from '@expo-google-fonts/nunito';
 
-
 import theme, {} from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes'
+
 
 export default function App() {
   const [fontsLoaded] = useFonts ({
@@ -30,7 +32,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
