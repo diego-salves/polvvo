61335490f5593ebeee33a036f7e0701ae625ac1e
Diego Santos <diegosantos57@outlook.com.br>
2021-10-25 23:34:06 -0300

    chama

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { Modal } from 'react-native';
+import { useForm } from 'react-hook-form';
 
 import { Input } from "../../components/Form/Input";
+import { InputForm } from "../../components/Form/InputForm";
 import { Button } from "../../components/Form/Button";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
@@ -17,6 +19,11 @@ import {
     TransactionsTypes,
  } from './styles';
 
+interface FormData {
+    name: string;
+    amount: string;
+}
+
 export function Register(){
     const [transactionType, setTransactionType] = useState(' ');
     const [categoryModalOpen, setCategoryModalOpen] = useState(false);
@@ -25,6 +32,12 @@ export function Register(){
         key: 'category',
         name: 'Categoria',
     });
+
+    const {
+        control, 
+        handleSubmit,
+
+    } = useForm();
 
     function handleTransactionTypeSelect(type: 'up' | 'down'){
         setTransactionType(type);
@@ -38,6 +51,17 @@ export function Register(){
         setCategoryModalOpen(false);
     }
 
+    function handleRegister(form: FormData){
+        const data= {
+            name: form.name,
+            amount: form.amount,
+            transactionType,
+            category: category.key
+        }
+
+       console.log(data);
+    }
+
     return(
         <Container>
             <Header>
@@ -49,10 +73,14 @@ export function Register(){
 
             <Form>
                 <Fields>
-                    <Input 
+                    <InputForm 
+                        name="name"
+                        control={control}
                         placeholder="Meta"
                     />
-                    <Input 
+                    <InputForm 
+                        name="amount"
+                        control={control}
                         placeholder="Recompensa"
                     />
 
@@ -77,7 +105,11 @@ export function Register(){
                     />
                 </Fields>
 
-                <Button title="Adicionar meta" />
+                <Button 
+                    title="Adicionar meta" 
+                    onPress={handleSubmit(handleRegister)}
+                />
+
             </Form>
             
             <Modal visible={categoryModalOpen}>
